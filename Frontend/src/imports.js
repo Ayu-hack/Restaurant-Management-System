f486685c3fead715484990568975a20675878343
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-09-30 14:35:27 +0530

    Add files via upload

---
+++ Frontend/src/imports.js
@@ -0,0 +1,7 @@
+import Home from "./views/pages/Home"
+import Cart  from "./views/components/cart"
+import Itemframes from "./views/components/itemframes"
+import Customers from "./views/components/Customers"
+import Orders from "./views/pages/Orders"
+
+export {Home,Orders,Cart,Itemframes,Customers}
