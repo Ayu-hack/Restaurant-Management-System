e818bc23cbcdd38b33738028da05e7e6d35d18d4
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-09-30 14:35:09 +0530

    app.jsx

---
+++ Frontend/src/app.jsx
@@ -0,0 +1,27 @@
+import {React,useContext} from "react"
+import {OrderProvider} from "./context/orderContext"
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
+import {Home,Orders} from "./imports"
+
+function App() {
+
+
+
+  
+  
+  return (
+    <>
+    <OrderProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/orders" element={<Orders />}></Route>
+          <Route path="*" element="<h1>404 NOT FOUND</h1>"> </Route>
+          </Routes>
+      </Router>
+    </OrderProvider>
+    </>
+  )
+}
+
+export default App
