3171a3d7ab82836ddc76af474f2077778612e2e6
Sirish C <chejerlasirish@gmail.com>
2024-10-05 02:02:24 -0500

    Added comments

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // server details enabling docker to run the application without issues
   server: {
     host: true,
     port: 8080, 
